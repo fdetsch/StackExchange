37bce57bf3f509cd815e6a8ce34d395374c4a6d9
fdetsch <fdetsch@web.de>
2018-05-11 11:51:57 +0200

    Update.

--- R/cropping-sf-object-in-r.R
+++ R/cropping-sf-object-in-r.R
@@ -1,19 +1,22 @@
-# Answer to 'Cropping sf object in R?' (added as comment to answer by @sebdelgarno) ----
+# Answer to 'Cropping sf object in R?' ----
 # (available online: https://gis.stackexchange.com/questions/282524/cropping-sf-object-in-r)
 
 library(cancensus)
 library(sf)
 
-options(cancensus.api_key = "CensusMapper_1939f5467f9ab0103dcdd03635321ff2")
+options(cancensus.api_key = "XXX") # insert own api key
 census_data <- get_census(dataset='CA16', regions=list(CMA="59933"),
                           vectors=c("v_CA16_408","v_CA16_409","v_CA16_410"),
                           level='CSD', geo_format = "sf", labels="short")
 
+## solution using st_intersection (not provided online)
 box = st_bbox(c(xmin = -122.9, xmax = -122.5, ymin = 49.1, ymax = 49.3)
               , crs = st_crs(4326))
 box = st_as_sfc(box) # see https://github.com/r-spatial/sf/issues/572
 
 ont_crop <- st_intersection(census_data, box)
-ont_join <- st_join(census_data, box)
 
+## solution using st_crop
+ont_crop <- st_crop(census_data
+                    , xmin = -122.9, xmax = -122.5, ymin = 49.1, ymax = 49.3)
 
